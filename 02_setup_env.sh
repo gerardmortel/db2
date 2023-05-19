640bdc2464658db8c2ee7e18925c52371800c378
Gerard Mortel <gerard@li-7f1bec4c-2af3-11b2-a85c-a3e8f9f7a463.ibm.com>
2023-05-19 08:52:25 -0500

    Updated namespace to cp4ba

--- 02_setup_env.sh
+++ 02_setup_env.sh
@@ -4,7 +4,7 @@
 # Get entitlement key from https://myibm.ibm.com/products-services/containerlibrary
 export API_KEY_GENERATED=""
 export USER_EMAIL="gmortel@us.ibm.com"
-export NS=db2
+export NS=cp4ba
 
 # Example DB2 CASE file info
 # https://github.com/IBM/cloud-pak/raw/master/repo/case/ibm-db2uoperator/5.1.4/ibm-db2uoperator-5.1.4.tgz
