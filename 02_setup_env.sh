23ab4b12fc58a662efab5f84a6f874ecec627fb1
Gerard Mortel <gmortel@us.ibm.com>
2023-10-11 17:43:52 -0500

    Clear out values

--- 02_setup_env.sh
+++ 02_setup_env.sh
@@ -4,14 +4,14 @@
 
 # Get entitlement key from https://myibm.ibm.com/products-services/containerlibrary
 export API_KEY_GENERATED=""
-export USER_EMAIL="gmortel@us.ibm.com"
-export NS=cp4ba
+export USER_EMAIL=""
+export NS=""
 
 # Example DB2 CASE file info
 # https://github.com/IBM/cloud-pak/raw/master/repo/case/ibm-db2uoperator/5.1.4/ibm-db2uoperator-5.1.4.tgz
-export CASE_NAME=ibm-db2uoperator
-export CASE_VERSION=5.1.4
-export OFFLINECASE=/tmp/case
-export CASEPATH="https://github.com/IBM/cloud-pak/raw/master/repo/case"
+export CASE_NAME=""
+export CASE_VERSION=""
+export OFFLINECASE=""
+export CASEPATH=""
 export OFFLINECASE=${OFFLINECASE}/${CASE_NAME}
 export CASE_ARCHIVE=${CASE_NAME}-${CASE_VERSION}.tgz
