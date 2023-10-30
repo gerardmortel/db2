a9d44977c4e5fa8e59f58d1d61dd613ce282be7d
Gerard Mortel <gmortel@us.ibm.com>
2023-10-30 08:57:47 -0500

    Put highest priority variables at top

--- 02_setup_env.sh
+++ 02_setup_env.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # Set up environment variables
-
-# Get entitlement key from https://myibm.ibm.com/products-services/containerlibrary
-export API_KEY_GENERATED=""
-export USER_EMAIL=""
-export NS=""
+export API_KEY_GENERATED="" # Get entitlement key from https://myibm.ibm.com/products-services/containerlibrary
+export USER_EMAIL="" # docker email address
+export NS="" # cp4ba
+export STORAGECLASS="" # managed-nfs-storage
 
 # Example DB2 CASE file info
 # https://github.com/IBM/cloud-pak/raw/master/repo/case/ibm-db2uoperator/5.1.4/ibm-db2uoperator-5.1.4.tgz
-export CASE_NAME=""
-export CASE_VERSION=""
-export OFFLINECASE=""
-export CASEPATH=""
+export CASE_NAME="" # ibm-db2uoperator
+export CASE_VERSION="" # 5.1.4
+export OFFLINECASE="" #/tmp/case
+export CASEPATH="" # https://github.com/IBM/cloud-pak/raw/master/repo/case
 export OFFLINECASE=${OFFLINECASE}/${CASE_NAME}
 export CASE_ARCHIVE=${CASE_NAME}-${CASE_VERSION}.tgz
-
-export STORAGECLASS=""
