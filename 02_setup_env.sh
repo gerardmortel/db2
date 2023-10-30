7ad589574dc67c0b0192e4512988043917c3d60f
Gerard Mortel <gmortel@us.ibm.com>
2023-10-30 09:31:25 -0500

    Added comments for high priority variables

--- 02_setup_env.sh
+++ 02_setup_env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Set up environment variables
+# Higher priority variables
 export API_KEY_GENERATED="" # Get entitlement key from https://myibm.ibm.com/products-services/containerlibrary
 export USER_EMAIL="" # docker email address
 export NS="" # cp4ba
 export STORAGECLASS="" # managed-nfs-storage
 
+# Lower priority variables
 # Example DB2 CASE file info
 # https://github.com/IBM/cloud-pak/raw/master/repo/case/ibm-db2uoperator/5.1.4/ibm-db2uoperator-5.1.4.tgz
 export CASE_NAME="" # ibm-db2uoperator
